7815976cee00a7ba4d21adbfa806a6516e9d710d
Tim Ebert <timebertt@gmail.com>
2021-06-03 10:11:00 +0200

    Disable WiFi warning message

--- build.sh
+++ build.sh
@@ -214,6 +214,27 @@ chmod +x 00-run-chroot.sh
 
 popd
 
+### add rfkill step to stage2
+step="12-rfkill"
+if [ -d "$step" ]; then
+  rm -Rf $step
+fi
+mkdir $step && pushd $step
+
+# must run after stage2/02-net-tweaks (which installs the wifi-check.sh script)
+cat > 00-run-chroot.sh <<"EOF"
+#!/bin/bash
+
+# disable warning message on login about WiFi being blocked by rfkill
+# WiFi is disabled by default, see https://github.com/RPi-Distro/pi-gen/blob/66cd2d17a0d2d04985b83a2ba830915c9a7d81dc/export-noobs/00-release/files/release_notes.txt#L223-L229
+if [ -e /etc/profile.d/wifi-check.sh ] ; then
+  mv /etc/profile.d/wifi-check.sh /etc/profile.d/wifi-check.sh.bak
+fi
+EOF
+chmod +x 00-run-chroot.sh
+
+popd
+
 # end modifying stage2
 popd
 
